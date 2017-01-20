eb452bee5ed6c21dc72488455230920e8036efe0
Drew Atkin <datkin@gmail.com>
2017-01-19 23:06:05 -0500

    its working

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-gcc original.c
+# The code overloads longs (32 bits) and pointers. We need to compile for a 32
+# bit architecture to ensure pointers don't exceed the 32 bit address range.
+gcc -g original.c -m32
